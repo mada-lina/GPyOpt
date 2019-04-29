Finally fix the computation of gradients for the acquisition LCBtarget_oneq

GPyOpt/acquisitions/LCB_oneq.py
@@ -61,12 +61,36 @@ class AcquisitionLCB_oneq(AcquisitionBase):
         msigma, vsigma, dmsigmadx, dvsigmadx = (2*mp -1), 4*vp, 2*dmpdx, 4*dvpdx
         macq = self.coeff_dim * (1 + np.dot(msigma, self.tgt_sigmas))
         sacq = self.coeff_dim * np.sqrt(np.dot(vsigma, np.square(self.tgt_sigmas)))
+        
         dmacqdx = self.coeff_dim * np.einsum('j,ijk', self.tgt_sigmas, dmsigmadx)
-        dsacqdx = self.coeff_dim * np.einsum('j,ijk', np.square(self.tgt_sigmas), dvsigmadx)/(2*sacq)
+        dsacqdx = self.coeff_dim**2 * np.einsum('j,ijk', np.square(self.tgt_sigmas), dvsigmadx)/(2*sacq)
+        #dsacqdx =  np.einsum('j,ijk', np.square(self.coeff_dim * self.tgt_sigmas), dvsigmadx * vsigma[:,:, np.newaxis]) /sacq
+        f_acqu = macq + self.exploration_weight * sacq       
+        df_acqu = dmacqdx + self.exploration_weight * dsacqdx
+        return f_acqu[:, np.newaxis], df_acqu[:, np.newaxis]
+        
+#        eps = 1e-6
+#        x_eps = x + eps * np.ones(np.shape(x))
+#        m_eps, s_eps, dmdx_eps, dsdx_eps = self.model.predict_withGradients(x_eps)
+#        mp_eps, vp_eps, dmpdx_eps, dvpdx_eps = change_of_var_Phi_withGradients(m_eps, s_eps, dmdx_eps, dsdx_eps)
+#        (m_eps - m) /eps
+#        np.sum(dmdx, 1)
+#        (mp_eps - mp) /eps
+#        np.sum(dmpdx, 2)
+#        (vp_eps - vp) /eps
+#        np.sum(dvpdx, 2)
+#        msigma_eps, vsigma_eps, dmsigmadx_eps, dvsigmadx_eps = (2*mp_eps -1), 4*vp_eps, 2*dmpdx_eps, 4*dvpdx_eps
+#        (msigma_eps - msigma)/eps
+#        np.sum(dmsigmadx, 2)
+#        macq_eps = self.coeff_dim * (1 + np.dot(msigma_eps, self.tgt_sigmas))
+#        sacq_eps = self.coeff_dim * np.sqrt(np.dot(vsigma_eps, np.square(self.tgt_sigmas)))
+#        dmacqdx_eps = self.coeff_dim * np.einsum('j,ijk', self.tgt_sigmas, dmsigmadx_eps)
+#        dsacqdx_eps = self.coeff_dim * np.einsum('j,ijk', np.square(self.tgt_sigmas), dvsigmadx_eps)/(2*sacq_eps)
+#
 
-        f_acqu = -macq + self.exploration_weight * sacq       
-        df_acqu = -dmacqdx + self.exploration_weight * dsacqdx
-        return f_acqu[:, np.newaxis], df_acqu
+
+
+
 
     def _compute_acq_novar(self, x):
         """

GPyOpt/acquisitions/base.py
@@ -57,7 +57,7 @@ class AcquisitionBase(object):
             #X_ext = multioutput.extend_X(x) 
             tmp, dtmp = self._compute_acq_withGradients(x)
             #f_acqu = multioutput.gather_Y(tmp, self.nb_output, target_len = len(x))
-            f_acqu, df_acqu = np.average(tmp, 1)[:, np.newaxis], np.average(dtmp, 1)[:, np.newaxis]
+            f_acqu, df_acqu = np.average(tmp, 1)[:, np.newaxis], np.average(dtmp, 1)
         else:
             f_acqu, df_acqu = self._compute_acq_withGradients(x)
 

GPyOpt/util/general.py
@@ -290,14 +290,15 @@ def change_of_var_Phi_withGradients(m, s, dmdx, dsdx):
     mcdf = stats.norm.cdf(a)
     vcdf = mcdf - np.square(mcdf) - 2 * special.owens_t(a, h)
     # Carefull 
+    
     dadx = dmdx / np.sqrt(1+v[:,:, None]) - a[:,:, None] * s[:,:, None] * dsdx / (1+v[:,:, None])
     #dhdx = - 2 * s * dsdx * h / (1+2*v)
     A = np.exp(-np.square(a[:,:, None])/2)
     dmcdfdx = 1/np.sqrt(2*np.pi)* A * dadx
     dTda = - A /(2*np.sqrt(2* np.pi)) * erf(a*h/np.sqrt(2))[:,:, None]
-    dTdh = -1/(2*np.pi *(1+v[:,:, None])) * np.exp(-np.square(m*h))[:,:, None] * h[:,:, None] * s[:,:, None] * dsdx
-    dTdx = dTda + dTdh
-
+    dTdhdhdx = - (np.exp(-np.square(m*h))[:,:, None] * h[:,:, None] * s[:,:, None] * dsdx) /(2*np.pi *(1+v[:,:, None]))
+    
+    dTdx = dTda * dadx  + dTdhdhdx
     dvcdfdx = dmcdfdx - 2 * mcdf[:,:, None] * dmcdfdx - 2 * dTdx 
 
     return mcdf, vcdf, dmcdfdx, dvcdfdx