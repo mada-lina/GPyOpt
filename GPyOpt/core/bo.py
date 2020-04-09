83f1a92645d5a4b7ac6692124cebc7ffbc58055c
Fred <Sauvage>
2020-04-09 15:06:56 +0100

    tabulation fix

--- GPyOpt/core/bo.py
+++ GPyOpt/core/bo.py
@@ -77,7 +77,9 @@ class BO(object):
 
         return suggested_locations
 
-    def run_optimization(self, max_iter = 0, max_time = np.inf,  eps = 1e-8, context = None, verbosity=False, save_models_parameters= True, report_file = None, evaluations_file = None, models_file=None):
+    def run_optimization(self, max_iter = 0, max_time = np.inf,  eps = 1e-8, context = None, 
+                         verbosity=False, save_models_parameters= True, report_file = None, 
+                         evaluations_file = None, models_file=None):
         """
         Runs Bayesian Optimization for a number 'max_iter' of iterations (after the initial exploration data)
 
